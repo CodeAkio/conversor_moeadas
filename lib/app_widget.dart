b6e767ce043d7b8a6878561b5c8539cd399e9d9d
CodeAkio <siqueira_2004@hotmail.com>
2021-09-27 16:37:37 -0300

    Basic structure

---
+++ lib/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:conversor_moedas/modules/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Conversor de Moedas",
+      home: HomePage(),
+    );
+  }
+}
